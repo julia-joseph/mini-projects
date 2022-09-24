aeab86509203ef952ae959969bd20d13b9c3e9a1
julia-joseph <juljoe726@gmail.com>
2022-09-24 19:33:56 +0530

    Project 16: Drink Water - Self Logic

--- src/app/drink-water/drink-water.component.ts
+++ src/app/drink-water/drink-water.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-drink-water',
   templateUrl: './drink-water.component.html',
-  styleUrls: ['./drink-water.component.scss']
+  styleUrls: ['./drink-water.component.scss'],
 })
 export class DrinkWaterComponent implements OnInit {
+  remaining: number = 0;
+  amount: number = 100;
+  totalGlassHeight: number = 400;
+  waterHeight: number = 0;
+  airHeight: number = 400;
+  refills = [
+    { amount: 250, full: false },
+    { amount: 250, full: false },
+    { amount: 250, full: false },
+    { amount: 250, full: false },
+    { amount: 250, full: false },
+    { amount: 250, full: false },
+    { amount: 250, full: false },
+    { amount: 250, full: false },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.setAmounts();
+    this.onResize();
   }
 
+  @HostListener('window:resize', ['$event']) onResize() {
+    if (window.innerHeight < 580) {
+      this.totalGlassHeight = 200;
+    }
+  }
+
+  fill(i: number) {
+    this.refills[i].full = !this.refills[i].full;
+    this.setAmounts();
+  }
+
+  setAmounts() {
+    const totalAmount = this.getTotalAmount();
+    const percentFull = totalAmount / 2000;
+    this.amount = (totalAmount / 2000) * 100;
+    this.remaining = (2000 - totalAmount) / 1000;
+    this.waterHeight = this.totalGlassHeight * percentFull;
+    this.airHeight = this.totalGlassHeight - this.waterHeight;
+  }
+
+  getTotalAmount() {
+    return this.refills.filter((refill) => refill.full).length * 250;
+  }
 }
